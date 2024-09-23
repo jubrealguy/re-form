d6332d37db527b4b4afc8b6441a9b4e50465ec34
jubrealguy <whizkidjubrealguy@gmail.com>
2024-09-23 16:35:58 +0100

    worked on the event page

--- src/Event.js
+++ src/Event.js
@@ -1,26 +1,74 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { faShareNodes } from '@fortawesome/free-solid-svg-icons';
 import reform_img from './img/reform_img.png'
+import conference_img from './img/conference_img.png'
 
 const Event = () => {
     return (
-        <div className="font-sans text-custom-gray">
-            <h2 className="text-4xl font-bold leading-[55.2px] text-center">REFORM TEACHERS CONFERENCE</h2>
+        <div className="events">
+            <h2 className="header__primary">REFORM TEACHERS CONFERENCE</h2>
 
-            <p className="text-[40px] font-bold leading-[46px] text-left">Upcoming Event</p>
-
-            <div class="flex gap-[115px] items-center">
-                <img src={reform_img} alt='' className="w-[426px] h-[424px]" />
-                <div className='flex flex-col gap-[27px]'>
-                    <h4 class="text-[55px] font-normal leading-[63.24px] text-custom-blue">Re-form Teacher Conference</h4>
-                    <p class="text-[18px] font-normal leading-[26.4px] text-custom-blue-light">Re-form Conference is an annual gathering of passionate edcators dedicated to advancing the future of teaching and learning,our conference brings together teachers, administrators, and education leaders from around the world to explore innovative strategies, share best practices,</p>
-                    <div className='flex justify-between'>
-                        <a href="##" class="text-[16px] font-bold leading-[20.16px] tracking-[0.02em] px-[16px] py-[8px] gap-[10px] rounded-[8px] bg-custom-link-background text-link-custom-text">Register here</a>
-                        <p>Date: 03 oct 2024</p>
+            <div class="event__info">
+                <p className="event__info--head">Upcoming Event</p>
+                <div className='event__info--body'>
+                    <div className='event__img-box'>
+                        <img src={reform_img} alt='' className='event__img' />
+                    </div>
+                    <div className="event__details">
+                        <h4 className="event__details--head">Re-form Teacher Conference</h4>
+                        <p className="event__details--body">Re-form Conference is an annual gathering of passionate edcators dedicated to advancing the future of teaching and learning,our conference brings together teachers, administrators, and education leaders from around the world to explore innovative strategies, share best practices,</p>
+                        <div className="event__details--register">
+                            <a href="##" className="register__link">Register here</a>
+                            <p className="register__date">Date: 03 oct 2024</p>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div className='px-[17px] py-0 shadow-custom-shadow'>
-                <h3>REGISTATION FORM</h3>
+            <form className="event__form">
+                <h3 className='event__form--head'>REGISTATION FORM</h3>
+                <p className='event__form--steps'>Steps</p>
+                <p className='event__form--guide'>Fill in the registration data to secure  a sit for the Teacher’s conference.</p>
+                <div className='form__box'>
+                    <label htmlFor='first' className='form__label'>First Name</label>
+                    <div className='input-box'>
+                        <input type='text' id='first' placeholder='John' className='form__input' />
+                        <FontAwesomeIcon icon={faPencilAlt} className='form__edit' />
+                    </div>
+                </div>
+                <div className='form__box'>
+                    <label htmlFor='last' className='form__label'>Last Name</label>
+                    <div className='input-box'>
+                        <input type='text' id='last' placeholder='Doe' className='form__input' />
+                        <FontAwesomeIcon icon={faPencilAlt} className='form__edit' />
+                    </div>
+                </div>
+                <div className='form__box'>
+                    <label htmlFor='email' className='form__label'>Email</label>
+                    <div className='input-box'>
+                        <input type='email' id='email' placeholder='abc@mail.xyz' className='form__input' />
+                        <FontAwesomeIcon icon={faPencilAlt} className='form__edit' />
+                    </div>
+                </div>
+                <div className='form__box'>
+                    <label htmlFor='phone' className='form__label'>Phone Number</label>
+                    <div className='input-box'>
+                        <input type='number' id='phone' placeholder='+234' className='form__input' />
+                        <FontAwesomeIcon icon={faPencilAlt} className='form__edit' />
+                    </div>
+                </div>
+                <button className='form__button'>Submit</button>
+            </form>
+
+            <div className='event__conference'>
+                <div className='event__conference--details'>
+                    <h4 className='conference__head'>Teachers Conference</h4>
+                    <p className='conference__date'>3rd of October 2024 | On site</p>
+                    <p className='conference__body'>Invite Fellow teachers, Childcare practitioners, school administrators, parents e.t.c</p>
+                    <a href='##' className='conference__share'><FontAwesomeIcon icon={faShareNodes} />Share</a>
+                </div>
+                <img src={conference_img} alt='' />
             </div>
         </div>
     )
